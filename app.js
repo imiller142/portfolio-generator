aca80d1f57ed8578d8d0d8e295ecb305593168ae
Ian Miller <imiller142@gmail.com>
2023-12-09 16:07:01 -0600

    add the ability to capture command line arguments.

---
+++ app.js
@@ -0,0 +1,11 @@
+let profileDataArgs = process.argv.slice(2, process.argv.length);
+
+console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+    for (let i = 0; i < profileDataArr.length; i++) {
+        console.log(profileDataArr[i])
+    }
+  };
+  
+  printProfileData(profileDataArgs)
